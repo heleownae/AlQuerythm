08ee5cf3658385ad16ac940b33058fde9ab61694
Haewon Lee <heleownae@gmail.com>
2024-03-31 22:06:11 +0900

    Create Q_ThePADS_LHW.sql

---
+++ 001주차/Q_ThePADS_LHW.sql
@@ -0,0 +1,10 @@
+-- HakerRank - The PADS (이해원)
+
+select concat(name, '(', left(occupation, 1), ')') 
+from occupations
+order by name;
+
+select concat('There are a total of ', count(name), ' ', lower(occupation), 's.')
+from occupations
+group by occupation
+order by count(occupation);
