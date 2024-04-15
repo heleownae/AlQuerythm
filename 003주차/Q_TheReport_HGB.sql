0c06145e23d3b12a47647a33ec1a6c05cfc4018b
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-04-15 13:34:03 +0900

    Create Q_TheReport_HGB.sql

---
+++ 003주차/Q_TheReport_HGB.sql
@@ -0,0 +1,6 @@
+SELECT IF(grades.grade > 7, students.name, 'NULL')
+            , grades.grade
+            , students.marks
+FROM students, grades 
+WHERE students.marks BETWEEN grades.min_mark AND grades.max_mark 
+ORDER BY grades.grade DESC, students.name
