9ddd8a9a75b661276170710e9d714ca94deaa498
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-04-17 19:24:06 +0900

    A_Card2_HGB.py

--- 003주차/A_Card2_HGB.py
+++ 003주차/A_Card2_HGB.py
@@ -1,10 +1,13 @@
 card = list(input())
     
 while card:
-    del card[0]
-    card.append(card[0])
-    del card[0]
     if len(card) == 1:
         break
+    else:
+        del card[0]
+        card.append(card[0])
+        del card[0]
+        if len(card) == 1:
+            break
     
 print(*card)
