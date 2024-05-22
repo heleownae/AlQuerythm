f8eeb7dde285e2b858fcb281631c751c133a183e
Haewon Lee <heleownae@gmail.com>
2024-05-22 21:25:26 +0900

    Create Q_Delivery_LHW.sql

---
+++ 007주차/Q_Delivery_LHW.sql
@@ -0,0 +1,8 @@
+-- 배송 예정일 예측 성공과 실패
+SELECT strftime('%Y-%m-%d', order_purchase_timestamp) as "purchase_date",
+  SUM(CASE WHEN order_estimated_delivery_date >= order_delivered_customer_date THEN 1 ELSE 0 END) as "success",
+  SUM(CASE WHEN order_estimated_delivery_date < order_delivered_customer_date THEN 1 ELSE 0 END) as "fail"
+FROM olist_orders_dataset
+WHERE strftime('%Y-%m-%d', order_purchase_timestamp) BETWEEN '2017-01-01' AND '2017-01-31'
+GROUP BY 1
+ORDER BY 1 asc;
