a5338085d7b298c892758f63b3a71bee2fd73122
Haewon Lee <heleownae@gmail.com>
2024-05-22 21:32:49 +0900

    Update Q_Delivery_LHW.sql

--- 007주차/Q_Delivery_LHW.sql
+++ 007주차/Q_Delivery_LHW.sql
@@ -3,6 +3,6 @@ SELECT strftime('%Y-%m-%d', order_purchase_timestamp) as "purchase_date",
   SUM(CASE WHEN order_estimated_delivery_date >= order_delivered_customer_date THEN 1 ELSE 0 END) as "success",
   SUM(CASE WHEN order_estimated_delivery_date < order_delivered_customer_date THEN 1 ELSE 0 END) as "fail"
 FROM olist_orders_dataset
-WHERE strftime('%Y-%m-%d', order_purchase_timestamp) BETWEEN '2017-01-01' AND '2017-01-31'
+WHERE strftime('%Y-%m-%d', order_purchase_timestamp) LIKE '2017-01%'
 GROUP BY 1
 ORDER BY 1 asc;
