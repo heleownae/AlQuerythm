c2f84e6362b7a1383bd145c95c05d9323c50518a
noel <157769634+augnoel@users.noreply.github.com>
2024-05-30 00:01:13 +0900

    Create Q_Weather20_JNE.sql

---
+++ 008주차/Q_Weather20_JNE.sql
@@ -0,0 +1,17 @@
+# 틀린 답임
+# 근데 푸는 방법이 더 이상 생각 안남
+SELECT ROUND(AVG(LAT_N), 2) AS median
+FROM (
+  SELECT LAT_N
+  FROM (
+    SELECT LAT_N, 
+           -- 각 행에 번호 할당
+           ROW_NUMBER() OVER (ORDER BY LAT_N) AS row_num,
+           -- 전체 행 수
+           COUNT(*) OVER() AS total_count
+    FROM STATION
+  ) AS with_row_numbers
+-- 중앙값 선택
+-- FLOOR 함수는 주어진 숫자 이하 중 가장 큰 정수를 반환하는 함수
+  WHERE row_num IN (FLOOR((total_count + 1) / 2), FLOOR((total_count + 2) / 2))
+) AS final_selection;
