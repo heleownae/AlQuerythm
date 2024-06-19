272d2ef598fbc2ea9f415fb3c8bc36e9676f8c79
ny1yyy <135477812+ny1yyy@users.noreply.github.com>
2024-06-20 00:11:17 +0900

    Create Average_PNY.py

---
+++ 011주차/Average_PNY.py
@@ -0,0 +1,9 @@
+# 1. ---------------------------------------------
+def solution(arr):     
+    return sum(arr)/len(arr)
+
+# 2. ---------------------------------------------
+import numpy as np
+
+def solution(arr):     
+    return np.mean(arr)
