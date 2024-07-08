0b02b74c2b9febe08b46d72cea030809d3afebdf
neddy0318 <152957560+neddy0318@users.noreply.github.com>
2024-07-08 09:59:05 +0900

    Update 음양더하기_SSH.py

--- 016회차/음양더하기_SSH.py
+++ 016회차/음양더하기_SSH.py
@@ -1,2 +1,5 @@
 def solution(absolutes, signs):
     return sum(absolutes[x] if signs[x] else absolutes[x]*(-1) for x in range(len(absolutes)))
+
+def solution(absolutes, signs):
+    return sum(absolute if sign else -absolute for absolute, sign in zip(absolutes, signs))
