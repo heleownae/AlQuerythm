5a81903f8f67f580c81660b9f1aa7944036bb674
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-07-09 13:53:08 +0900

    Update 가운데글자가져오기_BSH.py

--- 017회차/가운데글자가져오기_BSH.py
+++ 017회차/가운데글자가져오기_BSH.py
@@ -1,5 +1,3 @@
 def solution(s):
-    if len(s) % 2 == 1:
-        return list(s)[int(len(list(s))/2)]
-    else:
-        return list(s)[int(len(list(s))/2)-1] + list(s)[int(len(list(s))/2)]
+    n = int(len(list(s))/2)
+    return s[n] if len(s) % 2 == 1 else s[n-1:n+1]
