c79a17fd1fa797a5b1f93ec33ee0b7d337ba4c96
Haewon Lee <heleownae@gmail.com>
2024-07-17 11:55:46 +0900

    Update 문자열다루기기본_LHW.py

--- 019회차/문자열다루기기본_LHW.py
+++ 019회차/문자열다루기기본_LHW.py
@@ -1,7 +1,3 @@
 def solution(s):
     l = len(s)
-
-    if (l == 4 or l == 6) and s.isdecimal():
-        return True
-    else:
-        return False
+    return True if (l == 4 or l == 6) and s.isdecimal() else False
