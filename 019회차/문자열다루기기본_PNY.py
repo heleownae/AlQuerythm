9a6030cc966c3801361dd140646e3d9ed8ec00e8
ny1yyy <135477812+ny1yyy@users.noreply.github.com>
2024-07-18 00:53:43 +0900

    Update 문자열다루기기본_PNY.py

--- 019회차/문자열다루기기본_PNY.py
+++ 019회차/문자열다루기기본_PNY.py
@@ -1,15 +1,2 @@
 def solution(s):
     return (len(s) == 4 or len(s) == 6) and s.isdigit()
-
-'''
-# 예전 제출 코드
-def solution(s):
-    answer = True
-    if (len(s) == 4 or len(s) == 6) and s.isdigit():
-        pass
-    else:
-        answer = False
-    return answer
-
-# 논리연산자의 결과는 불리언 형식이라서 if문 필요 없음
-'''
