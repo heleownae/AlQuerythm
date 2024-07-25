e0030b577e90d931998ea68bf370a234524265db
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-07-25 10:59:49 +0900

    Update 이상한문자_BSH.py

--- 021회차/이상한문자_BSH.py
+++ 021회차/이상한문자_BSH.py
@@ -9,3 +9,19 @@ def solution(s):
             result.append(i.lower())
         num+=1
     return ''.join(result)
+
+# 재도전!
+def solution(s):
+    result=[]
+    num = 0
+    for i in s:
+        if i==' ':
+            result.append(' ')
+            num = 0
+        else:
+            if num%2==0:
+                result.append(i.upper())
+            else:
+                result.append(i.lower())
+            num+=1
+    return ''.join(result)
