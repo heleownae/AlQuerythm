72f112c68041e9541bd8d7f446e90b292758b582
Haewon Lee <heleownae@gmail.com>
2024-07-26 11:25:21 +0900

    Update 최소직사각형_LHW.py

--- 022회차/최소직사각형_LHW.py
+++ 022회차/최소직사각형_LHW.py
@@ -1,13 +1,13 @@
 def solution(sizes):
-    left = []
-    right = []
+    big = []
+    small = []
     
     for i in sizes:
         if i[0] >= i[1]:
-            left.append(i[0])
-            right.append(i[1])
+            big.append(i[0])
+            small.append(i[1])
         else:
-            right.append(i[0])
-            left.append(i[1])
+            small.append(i[0])
+            big.append(i[1])
     
-    return max(left) * max(right)
+    return max(big) * max(small)
