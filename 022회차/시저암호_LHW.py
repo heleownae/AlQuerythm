67fe77f00621daf233dbbc2d7571f7373a7c7354
Haewon Lee <heleownae@gmail.com>
2024-08-01 16:36:09 +0900

    Create 시저암호_LHW.py

---
+++ 022회차/시저암호_LHW.py
@@ -0,0 +1,12 @@
+def solution(s, n):
+    result = []
+    
+    for i in s:
+        if i.isupper():
+            result.append(chr((ord(i) - ord('A') + n) % 26 + ord('A')))
+        elif i.islower():
+            result.append(chr((ord(i) - ord('a') + n) % 26 + ord('a')))
+        else:
+            result.append(i)
+    
+    return ''.join(result)
