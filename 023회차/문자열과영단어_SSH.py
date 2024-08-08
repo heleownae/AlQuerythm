93365239f28f824d4d29da48616d14e0eeec96e7
Seunghyun Shin <152957560+neddy0318@users.noreply.github.com>
2024-08-08 10:25:22 +0900

    Create 문자열과영단어_SSH.py

---
+++ 023회차/문자열과영단어_SSH.py
@@ -0,0 +1,15 @@
+def solution(s):
+    num = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"}
+    for a, b in num.items():
+        s = s.replace(a, b)
+    return int(s)
