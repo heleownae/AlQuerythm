40a4a9c9b21ceafdb204f626dda5e55ca90c6269
Haewon Lee <heleownae@gmail.com>
2024-08-19 10:06:18 +0900

    Update FoodFight_LHW.py

--- 025회차/FoodFight_LHW.py
+++ 025회차/FoodFight_LHW.py
@@ -2,6 +2,6 @@ def solution(food):
     answer = ''
     
     for i in range(1, len(food)):
-        answer += (str(i)*(food[i]//2))
+        answer += (str(i) * (food[i] // 2))
     
     return answer + '0' + answer[::-1]
