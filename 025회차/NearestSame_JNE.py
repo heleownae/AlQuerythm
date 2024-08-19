a20b725b2100b05d33f702171ad4e349bdb57e90
noel <157769634+augnoel@users.noreply.github.com>
2024-08-19 10:26:44 +0900

    Update NearestSame_JNE.py

--- 025회차/NearestSame_JNE.py
+++ 025회차/NearestSame_JNE.py
@@ -4,16 +4,16 @@ def solution(s):
     # 결과 리스트 초기화
     result = []
     
-    for i, J in enumerate(s):
+    for i, j in enumerate(s):
         # 현재 문자의 마지막 위치가 기록되어 있는지 확인
-        if J in last:
+        if j in last:
             # 같은 글자가 이전에 나왔다면 현재 인덱스와 마지막 위치의 차이를 계산
-            result.append(i - last[J])
+            result.append(i - last[j])
         else:
             # 처음 나왔다면 -1 추가
             result.append(-1)
         
         # 현재 문자의 위치 업데이트
-        last[J] = i
+        last[j] = i
     
     return result
