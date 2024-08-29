5c9f363e1119d7b5362e9195c85fe0e565111b70
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-08-29 11:09:15 +0900

    Update 2016_BSH.py

--- 027회차/2016_BSH.py
+++ 027회차/2016_BSH.py
@@ -4,15 +4,12 @@ def solution(a, b):
     uu = 29  # 윤년
 
     # 일수를 다 더해서 나머지로 요일 계산한다.
-    dict = {1: 'FRI', 2: 'SAT', 3: 'SUN', 4: 'MON', 5: 'TUE', 6: 'WED', 0: 'THU'}
-
+    weekday = {1: 'FRI', 2: 'SAT', 3: 'SUN', 4: 'MON', 5: 'TUE', 6: 'WED', 0: 'THU'}
     # 각 월의 일수를 리스트로 만든다.
-    days_in_month = [0, jj, uu, jj, hh, jj, hh, jj, jj, hh, jj, hh, jj]
-
+    month = [0, jj, uu, jj, hh, jj, hh, jj, jj, hh, jj, hh, jj]
     # 전월까지의 총 일수 계산한다.
-    total_days = sum(days_in_month[1:a]) + (b)
-
+    total = sum(days_in_month[1:a]) + (b)
     # 총 일수를 7로 나눈 나머지를 통해 요일 찾는다.
-    weekday_index = (total_days) % 7
+    result = (total) % 7
 
-    return dict[weekday_index]
+    return weekday[result]
