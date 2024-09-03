0865291abf22dcda8457ddcf3cd9df09b26ea01d
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-09-03 15:07:58 +0900

    Create mock_test_BSH.py

---
+++ 029회차/mock_test_BSH.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    # 최대 10000개의 찍기 번호 리스트 만들기
+    a = [1,2,3,4,5] * 2000
+    b = [2,1,2,3,2,4,2,5] * 1250
+    c = [3,3,1,1,2,2,4,4,5,5] * 1000
+
+    # 정답 수 리스트
+    result = [0,0,0]
+
+    for i,j in enumerate(answers):
+        if a[i]==j:
+            result[0] += 1
+        if b[i]==j:
+            result[1] += 1
+        if c[i]==j:
+            result[2] += 1
+
+    # 최대로 맞은 사람들을 필터링하
+    max_result = max(result)
+
+    return [i+1 for i,j in enumerate(result) if j == max_result]
