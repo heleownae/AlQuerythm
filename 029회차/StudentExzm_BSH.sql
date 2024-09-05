0ebdb3109b83d328f207feeca3ae7bc3e1175326
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-09-05 10:15:37 +0900

    Create StudentExzm_BSH.sql

---
+++ 029회차/StudentExzm_BSH.sql
@@ -0,0 +1,14 @@
+SELECT  ST.student_id
+        , ST.student_name
+        , SU.subject_name
+        , COUNT(EX.subject_name) AS attended_exams
+FROM Students AS ST
+CROSS JOIN Subjects AS SU
+LEFT JOIN Examinations AS EX
+ON ST.student_id = EX.student_id
+AND SU.subject_name = EX.subject_name
+GROUP BY ST.student_id
+        , ST.student_name
+        , SU.subject_name
+ORDER BY ST.student_id
+        , SU.subject_name;
