3078414a15a0b90b4f3c747d378c401be6aecaac
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-09-10 19:37:04 +0900

    Create weapon_BSH.py

---
+++ 030회차/weapon_BSH.py
@@ -0,0 +1,8 @@
+def solution(number, limit, power):
+    def count_factor(n):
+        return sum(1 + (i != n // i) for i in range(1, int(n ** 0.5) + 1) if n % i == 0)
+
+    return sum(power if count_factor(i) > limit else count_factor(i) for i in range(1, number + 1))
+
+# 1. 약수의 개수 합을 구하는 함수를 만든다.
+# 2. if else문으로 필터링을 한다.
