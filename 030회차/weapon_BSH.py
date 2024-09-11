086e50f97387b821cf4d91164ccee5099a980130
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-09-11 10:23:26 +0900

    Update weapon_BSH.py

--- 030회차/weapon_BSH.py
+++ 030회차/weapon_BSH.py
@@ -1,6 +1,6 @@
 def solution(number, limit, power):
     def count_factor(n):
-        return sum(1 + (i != n // i) for i in range(1, int(n ** 0.5) + 1) if n % i == 0)
+        return sum(2 if n % i == 0 and i != n // i else 1 for i in range(1, int(n**0.5) + 1) if n % i == 0)
 
     return sum(power if count_factor(i) > limit else count_factor(i) for i in range(1, number + 1))
 
