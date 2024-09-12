d9292af7b80a3ffb7b0759c45d7728cc324018dd
ny1yyy <135477812+ny1yyy@users.noreply.github.com>
2024-09-12 10:28:03 +0900

    Update weapon_PNY.py

--- 030회차/weapon_PNY.py
+++ 030회차/weapon_PNY.py
@@ -1,6 +1,6 @@
 def c_divisor(n):  # 약수의 개수 구하는 함수
     count = 0
-    for i in range(1, int(n**(1/2)) +1):
+    for i in range(1, int(n**(1/2)) + 1):
         if n % i == 0:
             count += 1
             if i != n // i:
