78a20f9991d4f6b59d54f3eb3b6d1811415e2d98
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-09-19 01:17:05 +0900

    Create ConfirmationRate_BSH.sql

---
+++ 031회차/ConfirmationRate_BSH.sql
@@ -0,0 +1,7 @@
+SELECT SI.user_id
+       -- INFULL 없으면 NULL 떠서 추가 --
+       , IFNULL(ROUND(SUM(CO.action = 'confirmed') / COUNT(CO.action),2),0) AS confirmation_rate
+FROM Signups AS SI
+LEFT JOIN Confirmations AS CO
+ON SI.user_id = CO.user_id
+GROUP BY SI.user_id
