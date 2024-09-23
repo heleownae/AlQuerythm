fc7215cdb602f14b4d0ea5ba842a351d149b957e
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-09-23 10:00:06 +0900

    Create partner_BSH.py

---
+++ 032회차/partner_BSH.py
@@ -0,0 +1,40 @@
+# 시간초과: index, remove 문제
+def solution(X, Y):
+    result=[]
+
+    for i in X:
+        i = int(i)
+        Y=list(map(int, Y))
+        try:
+            Y.index(i)
+            Y.remove(i)
+            result.append(i)
+        except:
+            pass
+
+    if result==[]:
+        return "-1"
+    elif max(result)==0:
+        return "0"
+    else:
+        result.sort(reverse=True)
+        return ''.join(map(str,result))
+
+# 개선 방안
+from collections import Counter
+
+def solution(X, Y):
+    result = []
+    count_X = Counter(X)
+    count_Y = Counter(Y)
+
+    for digit in count_X:
+        if digit in count_Y:
+            result.extend([digit] * min(count_X[digit], count_Y[digit]))
+
+    if not result:
+        return "-1"
+    elif max(result) == '0':
+        return "0"
+    else:
+        return ''.join(sorted(result, reverse=True))
