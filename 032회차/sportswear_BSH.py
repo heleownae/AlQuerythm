a39058f65de3f35fe0c86e0c4b4a4d2c7531c811
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-09-24 20:42:41 +0900

    Update sportswear_BSH.py

--- 032회차/sportswear_BSH.py
+++ 032회차/sportswear_BSH.py
@@ -1,11 +1,9 @@
 def solution(n, lost, reserve):
     rest=sorted(reserve.copy())  # 1. 최댓값을 출력하기 위해 오름차순 정렬하기
-
     for i in reserve:
         if i in lost:
             lost.remove(i)
             rest.remove(i)  # 2. 여벌을 가져왔으나 도난 당한 학생 필터링하기
-
     for i in rest:
         cnt=0  # 3. 여벌번호에 해당하면 도난 당한 학생 리스트에서 제거하기
         if i-1 in lost:
@@ -13,5 +11,4 @@ def solution(n, lost, reserve):
             cnt+=1
         if cnt==0 and i+1 in lost:
             lost.remove(i+1)
-
     return n-len(lost)  # 4. 최종 학생수 출력하기
