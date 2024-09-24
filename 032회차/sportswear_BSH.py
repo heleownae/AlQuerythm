5f6d050e0a8e796a4e9496ae5dab9ac1f4c5c487
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-09-24 20:30:34 +0900

    Update sportswear_BSH.py

--- 032회차/sportswear_BSH.py
+++ 032회차/sportswear_BSH.py
@@ -1,1 +1,18 @@
 # 지독한 반례찾기
+def solution(n, lost, reserve):
+    rest=sorted(reserve.copy())  # 최댓값 출력을 위해 우선 정렬하기
+
+    for i in reserve:
+        if i in lost:
+            lost.remove(i)
+            rest.remove(i)  # 여벌을 가져왔으나 도난 당한 학생 필터링하기
+
+    for i in rest:
+        cnt=0
+        if i-1 in lost:
+            lost.remove(i-1)
+            cnt+=1
+        if cnt==0 and i+1 in lost:
+            lost.remove(i+1)
+
+    return n-len(lost)
