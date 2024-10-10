16e14bd08bd5e503d8441ec9c4619b2c8826f650
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-10-10 10:22:48 +0900

    Update percentage_BSH.sql

--- 036회차/percentage_BSH.sql
+++ 036회차/percentage_BSH.sql
@@ -1,8 +1,7 @@
-# 수정필요
 SELECT
     query_name,
     ROUND((SUM(rating/position))/COUNT(query_name),2) AS quality,
-    ROUND(((SELECT COUNT(query_name) FROM Queries WHERE rating<3)/COUNT(query_name))*100,2) AS poor_query_percentage
+    ROUND((COUNT(IF(rating<3,1,NULL))/COUNT(*))*100,2) AS poor_query_percentage
 FROM
     Queries
 GROUP BY
