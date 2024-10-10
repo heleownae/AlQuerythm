8a12bd797c288da84ebd0fc23a70fe9ad06a1e6e
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-10-10 10:09:32 +0900

    Create burger_BSH.py

---
+++ 036회차/burger_BSH.py
@@ -0,0 +1,9 @@
+# 수정 필요
+ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1]
+hamburger = [1,2,3,1]
+perpect = 0
+
+for i,e in enumerate(ingredient):
+    if e == 1:
+        if ingredient[i:i+4] is hamburger:
+            perpect+=1
