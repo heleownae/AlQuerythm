8c964a727090c5f3ef31491d8d00a4d7ec5cd618
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-10-13 16:43:40 +0900

    Update burger_BSH.py

--- 036회차/burger_BSH.py
+++ 036회차/burger_BSH.py
@@ -7,3 +7,15 @@ for i,e in enumerate(ingredient):
     if e == 1:
         if ingredient[i:i+4] is hamburger:
             perpect+=1
+
+# 수정 완료
+def solution(ingredient):
+    hamburger = [1,2,3,1]
+    temp = []
+    makeit = 0
+    for i in ingredient:
+        temp.append(i)
+        if temp[-4:] == hamburger:
+            makeit += 1
+            del temp[-4:]
+    return makeit
