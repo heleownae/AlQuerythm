eb42da6f38c2239b37df7cf9d7a9f9fe1fdf55c7
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-11-18 00:46:17 +0900

    Update JADEN_CASE_BSH.py

--- 044회차/JADEN_CASE_BSH.py
+++ 044회차/JADEN_CASE_BSH.py
@@ -1,1 +1,18 @@
 # 반례 s="  for the what  1what  "
+
+def solution(s):
+    result=''
+    flag=0
+
+    for i in s:
+        if flag==0 and i.isalpha():
+            flag=1
+            result=result+i.upper()
+        else:
+            result=result+i.lower()
+        if i == ' ':
+            flag=0
+        else:
+            flag=1
+
+    return result
