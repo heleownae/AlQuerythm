8a9ecd6b0605c323006c1709e41a82623fd2c7e4
noel <157769634+augnoel@users.noreply.github.com>
2024-12-09 16:15:06 +0900

    Create Fibonacci_JNE.py

---
+++ 046회차/Fibonacci_JNE.py
@@ -0,0 +1,11 @@
+def solution(n):
+    num = 1234567
+
+    fib = [0] * (n + 1)
+    fib[0] = 0
+    fib[1] = 1
+    
+    for i in range(2, n + 1):
+        fib[i] = (fib[i - 1] + fib[i - 2]) % num
+    
+    return fib[n]
