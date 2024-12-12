9a6976cde5adfd8bf13fe0e5e30522932f1d60cc
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-12-12 12:41:30 +0900

    Update Employees_BSH.sql

--- 047회차/Employees_BSH.sql
+++ 047회차/Employees_BSH.sql
@@ -5,7 +5,9 @@ SELECT
   ROUND(AVG(E2.age)) AS average_age
 FROM
   EMPLOYEES AS E1
-  JOIN EMPLOYEES AS E2
-  ON E1.employee_id = E2.reports_to
+JOIN
+  EMPLOYEES AS E2
+ON
+  E1.employee_id = E2.reports_to
 GROUP BY
   E1.employee_id, E1.name;
