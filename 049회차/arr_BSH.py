92c0c8d5540e12ce193660d955c246f3a0064b53
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-12-19 13:02:17 +0900

    Create arr_BSH.py

---
+++ 049회차/arr_BSH.py
@@ -0,0 +1,17 @@
+def solution(arr):
+    answer=0
+    arr=sorted(arr)
+    for i in range(len(arr)-1):
+        arr1 = arr.pop(-1)
+        arr2 = arr.pop(-1)
+        while True:
+            arr3 = arr1%arr2
+            if arr3 == 0:
+                answer=arr2
+                arr.append(arr2)
+                break
+            else:
+                arr1=arr2
+                arr2=arr3
+
+    # arr 모든수의 곱 / answer
