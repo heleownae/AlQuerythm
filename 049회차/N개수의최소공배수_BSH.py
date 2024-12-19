9dfaf857addb9761532d3aa15c45dcc1f329f02f
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-12-19 13:54:08 +0900

    Update and rename arr_BSH.py to N개수의최소공배수_BSH.py

---
+++ 049회차/N개수의최소공배수_BSH.py
@@ -0,0 +1,40 @@
+def solution(arr):
+    answer=0
+    arr=sorted(arr)
+    for i in range(len(arr)-1):
+        arr1 = arr.pop(-1)
+        arr2 = arr.pop(-1)
+        result = [arr1, arr2]
+        result = sorted(result)
+        arr1 = result[-1]
+        arr2 = result[-2]
+        tmp = arr1*arr2
+        print(arr1, arr2)
+        while True:
+            arr3 = arr1%arr2
+            if arr3 == 0:
+                answer = tmp//arr2
+                arr.append(answer)
+                break
+            else:
+                arr1=arr2
+                arr2=arr3
+
+    return answer
+
+# 더 쉬운 방법
+
+import math
+
+arr = [2, 6, 8, 14]
+
+# 1. 두 수씩 최소공배수를 계산한다.
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)  # 최대공약수 gcd
+
+# 2. 배열의 최소공배수를 계산한다.
+answer = arr[0]
+for num in arr[1:]:
+    answer = lcm(answer, num)
+
+print(answer)
