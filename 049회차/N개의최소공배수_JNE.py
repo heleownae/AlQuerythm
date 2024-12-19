fb6d5a487d6395d08b9a2a576f3fe3ec4c7502a3
noel <157769634+augnoel@users.noreply.github.com>
2024-12-19 14:26:12 +0900

    Update N개의최소공배수_JNE.py

--- 049회차/N개의최소공배수_JNE.py
+++ 049회차/N개의최소공배수_JNE.py
@@ -1,8 +1,10 @@
 import math
 
 def solution(arr):
+    # 1은 모든 자연수의 약수니까 일단 넣어놈
     a = 1
     for i in range(len(arr)):
         b = arr[i]
+        # arr에서 요소를 하나씩 받아와서 a와 b의 곱을 최대공약수로 나누는 과정 반복 
         a = abs(a * b) // math.gcd(a, b)
     return a
