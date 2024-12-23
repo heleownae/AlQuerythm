ffd140c195e79be1204fb1bc5611c0df12703c5f
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-12-23 12:24:17 +0900

    Update Consecutive_Numbers_BSH.sql

--- 050회차/Consecutive_Numbers_BSH.sql
+++ 050회차/Consecutive_Numbers_BSH.sql
@@ -1,10 +1,11 @@
 SELECT DISTINCT num as ConsecutiveNums
 FROM (
-    SELECT num, 
+    SELECT num,
+           --- 특정 범위의 같은 값을 지닌 행의 개수를 센다.---
            COUNT(*) OVER (PARTITION BY num, grp) AS consecutive_count
     FROM (
         SELECT num,
-                --- 연속적이지 않은 숫자를 가려낸다.---
+               --- 연속적이지 않은 숫자를 가려낸다.---
                ROW_NUMBER() OVER (ORDER BY id) - ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS grp
         FROM logs
     ) subquery1
