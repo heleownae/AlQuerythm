baec61bb953f82c1ac1a0d8bedc0722099f9825d
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-12-23 12:23:44 +0900

    Update Consecutive_Numbers_BSH.sql

--- 050회차/Consecutive_Numbers_BSH.sql
+++ 050회차/Consecutive_Numbers_BSH.sql
@@ -3,7 +3,8 @@ FROM (
     SELECT num, 
            COUNT(*) OVER (PARTITION BY num, grp) AS consecutive_count
     FROM (
-        SELECT num, 
+        SELECT num,
+                --- 연속적이지 않은 숫자를 가려낸다.---
                ROW_NUMBER() OVER (ORDER BY id) - ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS grp
         FROM logs
     ) subquery1
