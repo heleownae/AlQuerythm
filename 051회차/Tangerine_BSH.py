5943e4834f9ee13a277ce10940cbfcb1425894b2
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-12-26 13:01:18 +0900

    Update Tangerine_BSH.py

--- 051회차/Tangerine_BSH.py
+++ 051회차/Tangerine_BSH.py
@@ -2,7 +2,7 @@ from collections import Counter
 def solution(k, tangerine):
     result = 0
     a = Counter(tangerine)
-    for tan, cnt in a.most_common():
+    for tan, cnt in a.most_common():  # 요소의 빈도를 기준으로 정렬된 리스트를 반환한다.
         k -= cnt
         result += 1
         if k <= 0 : break
