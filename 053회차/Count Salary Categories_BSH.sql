30ee47218292dfb7a5cb339aa1317deb41569455
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2025-01-09 03:03:06 +0900

    Create Count Salary Categories_BSH.sql

---
+++ 053회차/Count Salary Categories_BSH.sql
@@ -0,0 +1,15 @@
+# 수정필요
+
+WITH Result AS (
+SELECT
+    CASE
+        WHEN income < 20000 THEN 'Low Salary'
+        WHEN 20000 <= income AND income <= 50000 THEN 'Average Salary'
+        WHEN income > 50000 THEN 'High Salary'
+    END AS category,
+    income
+FROM Accounts)
+
+SELECT category, COUNT(*) AS accounts_count
+FROM Result
+GROUP BY category
